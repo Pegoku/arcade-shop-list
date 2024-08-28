568fea39016008ed9070ebe621985a2c3d3fa365
Pegoku <pegoku08@gmail.com>
2024-08-28 12:25:58 +0200

    update

--- index.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const fs = require('fs');
-
-let nItems = 0;
-let nameItem = {};
-let priceItem = {};
-
-const url = "https://hackclub.com/_next/data/rUhEd7pIikv8meewfnRPN/arcade/shop.json";
-
