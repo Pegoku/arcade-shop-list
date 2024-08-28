32c3fa3bb688cbd20b047667b1e7f551299c78b2
Pegoku <pegoku08@gmail.com>
2024-08-28 12:14:43 +0200

    start

--- main.py
+++ main.py
@@ -1,13 +1,59 @@
 import requests
 import json
 
+nItems = 0
+nameItem = {}
+priceItem = {}
+
+
+# Example =
+# {
+#   "pageProps": {
+#     "availableItems": [
+#       {
+#         "Name": "USB-C Charger",
+#         "Small Name": "(30W)",
+#         "Full Name": "USB-C Charger (30W)",
+#         "Description": "Small, uses the latest GaN technology, & charges fast - pretty sweet!",
+#         "Fulfillment Description": "Available US only. \n",
+#         "Cost Hours": 6,
+#         "id": "rec0yAytJaSyye10S",
+#         "Image URL": "https://cloud-lit9nkas9-hack-club-bot.vercel.app/051t9lfjeuml._ac_sl1500_.png",
+#         "Max Order Quantity": 1,
+#         "Stock": 18,
+#         "Category": ["Hardware"]
+#       }, ...
+
+
 url = "https://hackclub.com/_next/data/rUhEd7pIikv8meewfnRPN/arcade/shop.json"
 
 response = requests.get(url)
 
 if response.status_code == 200:
     data = json.loads(response.text)
-    print(data)
+    with open('data.json', 'w') as file:
+        file.write(response.text)
+    # print(data)
 else:
     print("Error fetching data")
 
+# Sort the items by cost
+data['pageProps']['availableItems'].sort(key=lambda x: x['Cost Hours'])
+
+# Remove stock = 0
+data['pageProps']['availableItems'] = [item for item in data['pageProps']['availableItems'] if item['Stock'] != 0]
+
+# Get the Full name of all the items followed by the cost in hours
+for item in data['pageProps']['availableItems']:
+
+    nItems += 1
+    
+    nameItem[nItems] = item['Full Name']
+    priceItem[nItems] = item['Cost Hours']
+    
+    # print(f"{item['Full Name']} - {item['Cost Hours']} hours")
+
+for i in range(1, nItems+1):
+    print(f"{i} {nameItem[i]} - {priceItem[i]} hours")
+
+# print(nItems)
